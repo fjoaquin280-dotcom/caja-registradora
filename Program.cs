c8824c4dd6ff8f9bd940e7d71b7f72261e720577
fjoaquin280-dotcom <fjoaquin280@gmail.com>
2026-08-10 10:11:52 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
 ﻿Console.WriteLine("Hello, World!");
+string marketName = "open24";
+
+Console.Write("Ingrese su nombre: ");
+string userName = Console.ReadLine();
+Console.WriteLine($"Hola, {userName}. Bienvenido a {marketName}.");
+Console.ReadLine();
